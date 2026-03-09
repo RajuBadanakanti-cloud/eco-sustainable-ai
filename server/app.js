ad7eb83b07cd4ca84f01694e8b911d3275c98383
Raju Badanakanti <rajubadanakanti7@gmail.com>
2026-03-09 18:19:04 +0530

    Frontend Connection update

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ import errorMiddleWare from "./middleware/errorMiddleware.js"
 const app = express()
 app.use(helmet())
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: ["https://eco-sustainable-ai.vercel.app", "http://localhost:5173"], 
     credentials: true,
 }))
 app.use(express.json()) 
